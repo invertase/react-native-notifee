150b374b5f61df2069b08a872615703cce0eb50b
dackers86 <ackers86@hotmail.com>
2020-05-11 08:50:11 +0100

    Inital config and test passing

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  maxConcurrency: 10,
+  preset: './node_modules/react-native/jest-preset.js',
+  transform: {
+    '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFiles: ['./jest.setup.ts'],
+  testMatch: ['**/src/__tests__/**/*.test.(ts|js)'],
+  modulePaths: ['node_modules', 'src'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+};
