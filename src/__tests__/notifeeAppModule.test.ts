ab362be2f2ba7331225c340501e63b7d682c1f13
Salakar <mike.diarmid@gmail.com>
2020-05-11 14:17:37 +0100

    -

--- src/__tests__/notifeeAppModule.test.ts
+++ src/__tests__/notifeeAppModule.test.ts
@@ -1,8 +1,7 @@
-import React from 'react';
 import NotifeeApiModule from '../NotifeeApiModule';
 
 describe('Notifee App Module', () => {
-  test('functions configuration detected from environment variables', async () => {
+  test('TODO', async () => {
     expect(NotifeeApiModule).toBeDefined();
   });
 });
